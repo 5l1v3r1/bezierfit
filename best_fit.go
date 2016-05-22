2a028f38709a6ab0bf8085e62464e5ccc780762d
Alex Nichol <unixpickle@gmail.com>
2016-05-22 11:04:46 -0400

    variable step size for huge performance gain
    Both benchmarks got at least 5x faster.

--- best_fit.go
+++ best_fit.go
@@ -8,8 +8,9 @@ import (
 const (
 	bestFitAttempts   = 10
 	derivativeEpsilon = 1e-4
-	descentStepSize   = 0.1
 )
+
+var descentStepSizes = []float64{4, 1, 0.3, 0.1}
 
 // BestFit approximates the BezierAnimation of best
 // fit (in a least squares sense) for the list of
@@ -40,15 +41,20 @@ func BestFit(points []Point) *BezierAnimation {
 }
 
 func gradientDescent(points []Point, anim *BezierAnimation) {
+	var gradient [4]float64
 	paramPtrs := []*float64{&anim.P1.X, &anim.P1.Y, &anim.P2.X, &anim.P2.Y}
-	gradient := make([]float64, 4)
 	oldError := meanSquaredError(points, anim)
+
+	stepSizes := descentStepSizes
+
 	for {
+		var oldValues [4]float64
 		for i, ptr := range paramPtrs {
 			// Using this two-point formula gives O(derivativeEpsilon^2)
 			// error instead of the traditional derivative formula's
 			// O(derivativeEpsilon) error (and is thus more accurate).
 			centerVal := *ptr
+			oldValues[i] = centerVal
 			*ptr += derivativeEpsilon
 			rightErr := meanSquaredError(points, anim)
 			*ptr = centerVal - derivativeEpsilon
@@ -56,20 +62,34 @@ func gradientDescent(points []Point, anim *BezierAnimation) {
 			*ptr = centerVal
 			gradient[i] = (rightErr - leftErr) / (derivativeEpsilon * 2)
 		}
+		var bestError float64
+		var bestStepSize float64
+		for i, stepSize := range stepSizes {
+			for j, g := range gradient {
+				*paramPtrs[j] = oldValues[j] - g*stepSize
+				if j%2 == 0 {
+					*paramPtrs[j] = math.Max(0, math.Min(1, *paramPtrs[j]))
+				}
+			}
+			err := meanSquaredError(points, anim)
+			if i == 0 || err < bestError {
+				bestError = err
+				bestStepSize = stepSize
+			}
+		}
+		if bestError > oldError {
+			for i := range paramPtrs {
+				*paramPtrs[i] = oldValues[i]
+			}
+			return
+		}
 		for i, g := range gradient {
-			*paramPtrs[i] -= g * descentStepSize
-
-			// X parameters are bounded, so let's use a really
-			// crude active-set method.
+			*paramPtrs[i] = oldValues[i] - g*bestStepSize
 			if i%2 == 0 {
 				*paramPtrs[i] = math.Max(0, math.Min(1, *paramPtrs[i]))
 			}
 		}
-		newErr := meanSquaredError(points, anim)
-		if newErr > oldError {
-			break
-		}
-		oldError = newErr
+		oldError = bestError
 	}
 }
 
--- best_fit_test.go
+++ best_fit_test.go
@@ -31,11 +31,21 @@ func TestBestFitBounds(t *testing.T) {
 	}
 }
 
-func BenchmarkBestFit(b *testing.B) {
+func BenchmarkBestFitEasy(b *testing.B) {
 	p1 := Point{0.3, 0.2}
 	p2 := Point{0.7, 0.8}
 	p3 := Point{0.5, 0.56}
 	for i := 0; i < b.N; i++ {
 		BestFit([]Point{p1, p2, p3})
+	}
+}
+
+func BenchmarkBestFitHard(b *testing.B) {
+	p1 := Point{0.1, 0.3}
+	p2 := Point{0.2, 0.5}
+	p3 := Point{0.5, 0.8}
+	p4 := Point{0.8, 0.9}
+	for i := 0; i < b.N; i++ {
+		BestFit([]Point{p1, p2, p3, p4})
 	}
 }
