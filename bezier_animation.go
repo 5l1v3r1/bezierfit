afb915dac3e24a92518d19ec1f71f64e19a44d44
Alex Nichol <unixpickle@gmail.com>
2016-05-22 19:11:37 +0000

    tried newton's method; no performance gain

--- bezier_animation.go
+++ bezier_animation.go
@@ -1,6 +1,8 @@
 package bezierfit
 
-const evalIterations = 30
+import "math"
+
+const evalXPrecision = 1e-8
 
 type Point struct {
 	X float64
@@ -21,27 +23,45 @@ type BezierAnimation struct {
 // value along the bezier animation.
 // The given x value should be in the range [0,1].
 func (b *BezierAnimation) Eval(x float64) float64 {
-	var minT, maxT float64
-	maxT = 1
+	var approx, counter float64
+	var approxVal, counterVal float64
 
-	// Basic bisection search gives O(evalIterations) precision.
-	// If this were replaced with Newton's method, we could get
-	// O(evalIterations^2) precision (which is better).
-	for i := 0; i < evalIterations; i++ {
-		midT := (minT + maxT) * 0.5
-		xVal := b.xForT(midT)
-		if xVal < x {
-			minT = midT
-		} else if xVal > x {
-			maxT = midT
+	if x < 0.5 {
+		counter = 1
+		counterVal = 1
+	} else {
+		approx = 1
+		approxVal = 1
+	}
+
+	// Use a hybrid of Newton's method and bisection.
+	for math.Abs(approxVal-x) > evalXPrecision {
+		bisectionPoint := (approx + counter) / 2
+		newtonPoint := approx - (b.xForT(approx)-x)/(b.xPrimeForT(approx))
+
+		var usePoint float64
+		if math.Abs(approx-newtonPoint) < math.Abs(approx-bisectionPoint) {
+			usePoint = newtonPoint
 		} else {
-			maxT = midT
-			minT = midT
-			break
+			usePoint = bisectionPoint
+		}
+		useValue := b.xForT(usePoint)
+
+		if (useValue < x) == (approxVal < x) {
+			approx = usePoint
+			approxVal = useValue
+		} else {
+			counter = usePoint
+			counterVal = useValue
+		}
+
+		if math.Abs(approx-x) > math.Abs(counter-x) {
+			counter, approx = approx, counter
+			counterVal, approxVal = approxVal, counterVal
 		}
 	}
 
-	return b.yForT((minT + maxT) / 2)
+	return b.yForT(approx)
 }
 
 // xForT evaluates the Bezier curve's underlying
@@ -56,6 +76,13 @@ func (b *BezierAnimation) yForT(t float64) float64 {
 	return cubicBezierParametricEquation(0, b.P1.Y, b.P2.Y, 1, t)
 }
 
+// xPrimeForT evaluates the derivative of the
+// underlying parametric equation for x with
+// respect to t.
+func (b *BezierAnimation) xPrimeForT(t float64) float64 {
+	return cubicBezierDerivative(0, b.P1.X, b.P2.X, 1, t)
+}
+
 func cubicBezierParametricEquation(p0, p1, p2, p3, t float64) float64 {
 	tComp := 1 - t
 	tComp2 := tComp * tComp
@@ -63,4 +90,11 @@ func cubicBezierParametricEquation(p0, p1, p2, p3, t float64) float64 {
 	t2 := t * t
 	t3 := t2 * t
 	return tComp3*p0 + 3*tComp2*t*p1 + 3*tComp*t2*p2 + t3*p3
+}
+
+func cubicBezierDerivative(p0, p1, p2, p3, t float64) float64 {
+	tComp := 1 - t
+	tComp2 := tComp * tComp
+	t2 := t * t
+	return 3*tComp2*(p1-p0) + 6*tComp*t*(p2-p1) + 3*t2*(p3-p2)
 }
